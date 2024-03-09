7a41a7d45336ddeca4ad452247d695b7ee484f39
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-08 21:09:55 -0800

    first run

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 int main()
 {
-   
-   /* Complete your code */
-  
+   int N;
+   cin>>N;
+   int f;
+   char a;
+   for (int j = 0; j < N; j++)
+   {
+      f=41;
+      for (size_t i = 0; i < j; i++)
+      {
+         a=f;
+         cout<<a;
+      }
+   }
+   cout<<endl;
+   return 0;
 }

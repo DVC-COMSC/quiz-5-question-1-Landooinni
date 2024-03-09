413766a623a25ebd0e902a52cab3fe4ef5bcc63b
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-08 21:15:36 -0800

    fix

--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@ int main()
    char a;
    for (int j = 0; j < N; j++)
    {
-      f=41;
-      for (size_t i = 0; i < j; i++)
+      f=65;
+      for (size_t i = 0; i < (j+1); i++)
       {
          a=f;
+         f=f+1;
          cout<<a;
       }
+      cout<<"\n";
    }
    cout<<endl;
    return 0;
